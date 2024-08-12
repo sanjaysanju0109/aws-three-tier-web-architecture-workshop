4b56480f9c9b7cfb0caf08c5b748ca0dbf2fdd43
sanjaysanju0109 <sanjay.19ec27@gmail.com>
2024-08-13 00:16:34 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cjcqw4ca61z4.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Sanjay123',
+    DB_DATABASE : 'webappdb'
 });
